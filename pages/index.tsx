31091e3ab8cda671299f4a758441482976a76ccd
birdgang <birdgang82@gmail.com>
2023-09-19 18:05:04 +0900

    add Next Head component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import Head from "next/head";
+
 function HomePage() {
     console.log('HomePage ender');
     return (
         <>
+            <Head>
+                <title>My Blog</title>
+                <meta name="description" value="This is my blog" />
+            </Head>
             <main>
                 <h1>My Blog</h1>
             </main>
