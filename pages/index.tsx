0d59a031a07d4524ed65afec5177e0d72fd2eeda
birdgang <birdgang82@gmail.com>
2023-09-19 17:48:14 +0900

    add Next Link Component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
+import Link from "next/link";
+
 function HomePage() {
+    console.log('HomePage ender');
     return (
         <>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/about">
+                                About
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
             <main>
                 <h1>My Blog</h1>
             </main>
