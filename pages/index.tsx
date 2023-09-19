dc4e11e2d3863dc9c0c0a83bc261e5cc4ae0a553
birdgang <birdgang82@gmail.com>
2023-09-19 19:25:22 +0900

    add first post page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 function HomePage() {
     console.log('HomePage ender');
@@ -11,9 +12,11 @@ function HomePage() {
             <main>
                 <h1>My Blog</h1>
                 <ul>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
+                    <li>
+                        <Link href="/posts/first-post">
+                            First Post
+                        </Link>
+                    </li>
                 </ul>
             </main>
         </>
